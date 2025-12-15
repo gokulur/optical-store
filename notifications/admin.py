e329d46ac822bdb84912d6908e9db2e6d73a9d73
GOKUL UR <gokulkrishnagokul6@gmail.com>
2025-12-15 21:56:15 +0530

    notification admin section updated

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import NotificationRequest
 
+
 @admin.register(NotificationRequest)
 class NotificationRequestAdmin(admin.ModelAdmin):
-    list_display = ("variant", "requested_power", "notified", "created_at")
-    list_filter = ("notified",)
+    list_display = (
+        "id",
+        "variant",
+        "requested_power",
+        "user",
+        "email",
+        "notified",
+        "active",
+        "created_at",
+    )
+
+    list_filter = ("notified", "active")
+    search_fields = ("email", "variant__product__name")
+    readonly_fields = ("created_at", "notified_at")
