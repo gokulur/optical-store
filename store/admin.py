e3bdb55bc6e1533d6a71c5e5085397bf8da58c7d
GOKUL UR <gokulkrishnagokul6@gmail.com>
2025-12-15 21:51:45 +0530

    store admin section updated

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Cart, CartItem, Order, OrderItem, Payment
 
-# Register your models here.
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "created_at")
+    inlines = [CartItemInline]
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "status", "total_amount", "created_at")
+    list_filter = ("status",)
+    inlines = [OrderItemInline]
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("order", "provider", "amount", "success")
