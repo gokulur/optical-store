afbf9eeb0fbf0e674c07399cc6939e1356836a79
GOKUL UR <gokulkrishnagokul6@gmail.com>
2025-12-15 21:50:20 +0530

    inventory admin section updated

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Stock
 
-# Register your models here.
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("variant", "power", "quantity", "reserved", "last_updated")
+    list_filter = ("variant__product",)
+    search_fields = ("variant__product__name",)
