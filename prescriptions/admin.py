79d625b2f7c37c8b506a0e4de178336471dce5f0
GOKUL UR <gokulkrishnagokul6@gmail.com>
2025-12-15 21:51:00 +0530

    prescription admin section updated

--- prescriptions/admin.py
+++ prescriptions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Prescription
 
-# Register your models here.
+@admin.register(Prescription)
+class PrescriptionAdmin(admin.ModelAdmin):
+    list_display = ("user", "created_at")
+    search_fields = ("user__username",)
