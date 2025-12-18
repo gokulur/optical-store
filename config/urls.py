6239b7136342001b6c36f692847174b51db89899
GOKUL UR <gokulkrishnagokul6@gmail.com>
2025-12-18 19:14:41 +0530

    admin section updated

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from catalog.views import admin_dashboard  # We'll create this view
+from catalog.views import admin_dashboard  
 
 urlpatterns = [
     # Custom admin dashboard
