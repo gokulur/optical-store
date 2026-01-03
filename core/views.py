ee6723734b3204d9c613bc96823665343229fe67
GOKUL UR <gokulkrishnagokul6@gmail.com>
2026-01-03 23:09:02 +0530

    home updated

--- core/views.py
+++ core/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'base.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
