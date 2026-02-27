463647730169058c0239c0a27f046e1e8f0aae0e
GOKUL UR <gokulkrishnagokul6@gmail.com>
2026-02-27 19:16:41 +0530

    updated

--- content/models.py
+++ content/models.py
@@ -14,6 +14,7 @@ class Banner(models.Model):
         ('main_slider', 'Main Slider'),
         ('secondary', 'Secondary Banner'),
         ('sidebar', 'Sidebar'),
+        ('sale_banner',     'Sale Banner (Homepage)'),
     ]
     
     title = models.CharField(max_length=200)
